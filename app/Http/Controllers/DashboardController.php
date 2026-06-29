5fa3eba05e886bce506b80079b01ac3acaa819a3
Brandon07 <brandongiraldo8@gmail.com>
2026-06-29 16:36:21 -0500

    configurar rutas web y metodos de retorno en controladores

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        //
+        return view('dashboard.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReservationController;
 
+Route::get('/', DashboardController::class);
+Route::get('/usuarios', [UserController::class, 'index']);
+Route::get('/empleados', [EmployeeController::class, 'index']);
+Route::get('/clientes', [ClientController::class, 'index']);
+Route::get('/servicios', [ServiceController::class, 'index']);
+Route::get('/productos', [ProductController::class, 'index']);
+Route::get('/reservas', [ReservationController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
